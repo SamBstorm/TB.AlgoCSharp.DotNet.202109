b99378ae0f248b3eb3d818736db13417e2ebf5c7
Legrain Samuel <samuel.legrain@bstorm.be>
2021-09-29 16:36:12 +0200

    Ajoutez des fichiers projet.

---
+++ Exos01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exos01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nb1, nb2, temp;
+
+            Console.WriteLine("Veuillez indiquer le nombre a :");
+            nb1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Veuillez indiquer le nombre b :");
+            nb2 = int.Parse(Console.ReadLine());
+            temp = nb1;
+            nb1 = nb2;
+            nb2 = temp;
+            Console.Write("La valeur de a est ");
+            Console.WriteLine(nb1);
+            //Console.WriteLine("La valeur de b est {0}", nb2);
+            Console.WriteLine($"La valeur de b est {nb2}");
+        }
+    }
+}
---
+++ Exos02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exos02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d, e;
+            a = 8 % 3;
+            b = 4 + a;
+            c = b * a;
+            d = (c - a) * b;
+            e = ((a + 7) * (b / a)) * 0;    //DIV est une division entière, mais a et b étant des entier, aucun soucis!
+
+            Console.WriteLine($"La valeur de a est {a}.");
+            Console.WriteLine($"La valeur de b est {b}.");
+            Console.WriteLine($"La valeur de c est {c}.");
+            Console.WriteLine($"La valeur de d est {d}.");
+            Console.WriteLine($"La valeur de e est {e}.");
+        }
+    }
+}
