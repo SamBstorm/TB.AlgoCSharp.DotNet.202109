507662600a9fe99e01c50c35421955c97b38f1db
Legrain Samuel <samuel.legrain@bstorm.be>
2021-09-29 17:02:22 +0200

    Exos 03 : Ajout Version 1

--- Exos03/Program.cs
+++ Exos03/Program.cs
@@ -6,7 +6,29 @@ namespace Exos03
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region Version 1
+
+            int sec, min, heure, jour;
+            Console.WriteLine("Combien de secondes?");
+            sec = int.Parse(Console.ReadLine());
+            jour = sec / 86_400;
+            sec = sec % 86_400;
+            heure = sec / 3600;
+            sec = sec % 3600;
+            min = sec / 60;
+            sec = sec % 60;
+
+            Console.WriteLine($"Cela fait {jour} jour(s) {heure} : {min} : {sec}");
+
+            #endregion
+            #region Version 2
+
+            #endregion
+
+            #region Version 3
+
+            #endregion
+
         }
     }
 }
