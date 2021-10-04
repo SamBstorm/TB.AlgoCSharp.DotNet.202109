dc6c44f0e2db3d9934a374f368ab1ee0ae3a719c
Legrain Samuel <samuel.legrain@bstorm.be>
2021-10-04 15:18:26 +0200

    Add Exos09 & Exos11

---
+++ Exos09/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exos09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MULTIPLICATEUR = 2;
+            int nombre = 1;
+
+            while (nombre <= 10)
+            {
+                 System.Console.WriteLine(nombre + " x " + MULTIPLICATEUR + " = " + (nombre * MULTIPLICATEUR));
+                 nombre = nombre + 1;
+                 //nombre += 1;
+                 //nombre++;
+            }
+        }
+    }
+}
---
+++ Exos11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exos11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int multiplicateur = 1;
+            while (multiplicateur <= 9)
+            {
+                int nombre = 1;
+                while (nombre <= 10)
+                {
+                    Console.WriteLine(nombre + " x " + multiplicateur + " = " + (nombre * multiplicateur));
+                    nombre++;
+                }
+                multiplicateur++;
+            }
+        }
+    }
+}
