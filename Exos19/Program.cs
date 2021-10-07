b3ada0756e3d0950a478b975e0324dbe72d1424c
Legrain Samuel <samuel.legrain@bstorm.be>
2021-10-07 09:42:12 +0200

    Add Exos19 & Exos20

---
+++ Exos19/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Exos19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region VERSION SANS TRYPARSE
+            //const int TAILLE = 10;
+            //int[] tabScores = new int[TAILLE];
+            //int nbJoueurs;
+            //double moyenne = 0;
+
+            //do
+            //{
+            //    Console.WriteLine($"Veuillez indiquer le nombre de joueurs (2 à {TAILLE} joueurs) :");
+            //    nbJoueurs = int.Parse(Console.ReadLine());
+            //} while (nbJoueurs < 2 || nbJoueurs > TAILLE);
+
+            //for (int index = 0; index <= nbJoueurs - 1; index = index + 1)
+            //{
+            //    Console.WriteLine($"Veuillez indiquer le score du joueur {index + 1} :");
+            //    tabScores[index] = int.Parse(Console.ReadLine());
+            //}
+
+            //for (int index = 0; index <= nbJoueurs - 1; index = index + 1)
+            //{
+            //    moyenne = moyenne + tabScores[index];
+            //}
+
+            //Console.WriteLine($"La moyenne des scores de la partie est  {moyenne / nbJoueurs}");
+            #endregion
+            
+            
+            #region VERSION AVEC TRYPARSE
+            const int TAILLE = 10;
+            int[] tabScores = new int[TAILLE];
+            int nbJoueurs;
+            double moyenne = 0;
+
+            do
+            {
+                do
+                {
+                    Console.WriteLine($"Veuillez indiquer le nombre de joueurs (2 à {TAILLE} joueurs) :");
+                } while (!int.TryParse(Console.ReadLine(), out nbJoueurs));
+            } while (nbJoueurs < 2 || nbJoueurs > TAILLE);
+
+            for (int index = 0; index <= nbJoueurs - 1; index = index + 1)
+            {
+                do {
+                    Console.WriteLine($"Veuillez indiquer le score du joueur {index + 1} :");
+                } while (!int.TryParse(Console.ReadLine(), out tabScores[index]));
+            }
+
+            for (int index = 0; index <= nbJoueurs - 1; index = index + 1)
+            {
+                moyenne = moyenne + tabScores[index];
+            }
+
+            Console.WriteLine($"La moyenne des scores de la partie est  {moyenne / nbJoueurs}"); 
+            #endregion
+        }
+    }
+}
---
+++ Exos20/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Exos20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int TAILLE = 4;
+            int[] tab = new int[TAILLE];
+            int temp, index;
+
+            for (index = 0; index < TAILLE ; index++)
+            {
+                Console.WriteLine("Veuillez introduire un nombre : ");
+                tab[index] = int.Parse(Console.ReadLine());
+            }
+
+            for (index = 0; index < TAILLE / 2; index++)
+            {
+                temp = tab[index];
+                tab[index] = tab[(TAILLE - 1) - index];
+                tab[(TAILLE - 1) - index] = temp;
+            }
+
+            for (index = 0; index < TAILLE; index++)
+            {
+                Console.WriteLine(tab[index]);
+            }
+
+            #region Exemple FOREACH (POUR CHAQUE objet DU tableau)
+            //foreach (int number in tab)
+            //{
+            //    Console.WriteLine(number);
+            //} 
+            #endregion
+        }
+    }
+}
