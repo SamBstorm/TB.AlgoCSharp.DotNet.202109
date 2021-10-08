c811b36e512dae1e4706731455e312d609834aec
Legrain Samuel <samuel.legrain@bstorm.be>
2021-10-08 13:49:27 +0200

    Add Exos 28

---
+++ Exos28/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exos28
+{
+    class Program
+    {
+        public static int Carre(int nombre)
+        {
+            return nombre * nombre ;
+        }
+
+        static void Main(string[] args)
+        {
+            int nb, result;
+
+            nb = 2;
+            result = Carre(nb);
+
+            //OU
+
+            Console.WriteLine(Carre(2));
+        }
+    }
+}
