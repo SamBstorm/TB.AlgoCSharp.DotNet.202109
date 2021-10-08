9557cd4e41741cb7ad574504f482dfeeee436256
Legrain Samuel <samuel.legrain@bstorm.be>
2021-10-08 15:49:46 +0200

    Add DemoRandom

---
+++ DemoRandom/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DemoRandom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random RNG = new Random();
+
+            Console.WriteLine(RNG.Next(1,6));
+        }
+    }
+}
