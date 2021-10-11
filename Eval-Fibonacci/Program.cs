d4c5cbc2132255e80915e3513375ce03c1f85e4b
Legrain Samuel <samuel.legrain@bstorm.be>
2021-10-11 15:03:56 +0200

    Add Evals part1

---
+++ Eval-Fibonacci/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Eval_Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 0, fibo1 = 0, fibo2 = 1, nombre;
+
+            Console.WriteLine("Veuillez indiquer le nombre à vérifier : ");
+            nombre = int.Parse(Console.ReadLine());
+
+            //do Console.WriteLine("Veuillez indiquer le nombre à vérifier : ");
+            //while (!int.TryParse(Console.ReadLine(), out nombre));
+
+            while (nombre > fibo1)
+            {
+                Console.WriteLine(fibo1);
+                fibo2 += fibo1;
+                fibo1 = fibo2 - fibo1;
+                index++;
+            }
+
+            if (nombre == fibo1) Console.WriteLine($"{nombre} est bien un nombre de la suite de Fibonacci : F{index}");
+            else Console.WriteLine($"{nombre} n'est pas un nombre de la suite de Fibonacci");
+        }
+    }
+}
---
+++ Eval-Login/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Eval_Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string email, password, checkEmail, checkPassword;
+            int nbTentative = 3;
+            Console.WriteLine("Veuillez introduire votre e-mail :");
+            email = Console.ReadLine();
+            Console.WriteLine("Veuillez introduire votre mot de passe :");
+            password = Console.ReadLine();
+
+            do
+            {
+                Console.WriteLine("Veuillez vous authentifier");
+                Console.WriteLine($"{nbTentative} tentatives restantes");
+                Console.Write("e-mail : ");
+                checkEmail = Console.ReadLine();
+                Console.Write("mot de passe : ");
+                checkPassword = Console.ReadLine();
+                nbTentative--;
+            } while (((email != checkEmail) || (password != checkPassword)) && (nbTentative != 0));
+
+            if (email == checkEmail && password == checkPassword)
+            {
+                Console.WriteLine($"Bienvenu {email}");
+            }
+            else
+            {
+                Console.WriteLine("Accès refusé");
+            }
+        }
+    }
+}
